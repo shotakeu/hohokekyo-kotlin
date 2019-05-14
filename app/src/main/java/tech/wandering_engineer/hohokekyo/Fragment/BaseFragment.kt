b3ac68d8d1ec540f582df4a745dd39a645b514d4
Shotaro Takeuchi <taro20090819@yahoo.co.jp>
2019-05-14 16:38:48 +0900

    クラッシュ対応のつもりがtablayoutまで実装してしまった。

--- app/src/main/java/tech/wandering_engineer/hohokekyo/Fragment/BaseFragment.kt
+++ app/src/main/java/tech/wandering_engineer/hohokekyo/Fragment/BaseFragment.kt
@@ -1,12 +1,10 @@
 package tech.wandering_engineer.hohokekyo.Fragment
 
-import android.media.MediaPlayer
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_main.view.*
+import androidx.fragment.app.Fragment
 import tech.wandering_engineer.hohokekyo.R
 
 /**
